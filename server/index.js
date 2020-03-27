d2982d0155876f2c2276c6ba30c763fff81d4b4c
tomrobertazzi <62471807+tomrobertazzi@users.noreply.github.com>
2020-03-27 16:24:52 -0400

    new code

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there Matt Owen!' });
+});
+
+//app.get('/tom',(req, res) => {
+//    res.send({ tom: 'hi there' });
+//});
+
+// console.log(app);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
+
+
+// localhost:5000
