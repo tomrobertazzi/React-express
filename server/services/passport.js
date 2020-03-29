b04b94e37104d5f5deec02d3cb2bba247e16a874
tomrobertazzi <62471807+tomrobertazzi@users.noreply.github.com>
2020-03-28 21:01:53 -0400

    added cookies

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport")
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null,user);
+    });
+});
 
 passport.use(
     new GoogleStrategy(
@@ -10,10 +23,21 @@ passport.use(
             callbackURL: '/auth/google/callback'
         }, 
         (accessToken, refreshToken, profile, done) => {
-            console.log('access Token',accessToken);
-            console.log('refresh Token', refreshToken);
-            console.log('profile', profile);
+            User.findOne({ googleId: profile.id }).then((existingUser) => {
+                if (existingUser) {
+                    // we already have a record with given profile ID
+                    done(null, existingUser);
+                } else {
+                    // we don't have a user record with this ID, make a new record
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
+                }
+            });
         }
     )
 );
 
+//console.log('access Token',accessToken);
+//console.log('refresh Token', refreshToken);
+//console.log('profile', profile);
