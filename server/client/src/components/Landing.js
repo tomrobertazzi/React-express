e84cdf99fe8be1ec605bdd1bf636c327be69d9a2
tomrobertazzi <62471807+tomrobertazzi@users.noreply.github.com>
2020-03-30 22:54:14 -0400

    React client side dev

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center'}}>
+            <h1>
+                Emaily! (For Max and Theo)
+            </h1>
+            Collect feedback from your users
+        </div>
+    );
+};
+
+export default Landing;
