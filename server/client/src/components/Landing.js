3ee8fdd82e9cfdf66b7ff549637a44bc8d50eaac
tomrobertazzi <62471807+tomrobertazzi@users.noreply.github.com>
2020-04-01 20:05:34 -0400

    Setup server survey code and mailer

--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -4,7 +4,7 @@ const Landing = () => {
     return (
         <div style={{ textAlign: 'center'}}>
             <h1>
-                Emaily! (For Max and Theo)
+                Emaily!
             </h1>
             Collect feedback from your users
         </div>
