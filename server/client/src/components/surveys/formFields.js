87ab674f0b33df2735c4cdc98688ce16f8c93ef7
tomrobertazzi <62471807+tomrobertazzi@users.noreply.github.com>
2020-04-03 10:05:13 -0400

    Added in more features in react app

---
+++ server/client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+    { label: 'Survey Title', name: 'title', displayError: 'You must provide a title'},
+    { label: 'Subject Line', name: 'subject', displayError: 'You must provide a subject'},
+    { label: 'Email Body', name: 'body', displayError: 'You must provide a body'},
+    { label: 'Recipient List', name: 'recipients', displayError: 'You must provide an email'}
+];
